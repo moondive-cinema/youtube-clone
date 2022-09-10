12e3f15bf6f9f0e01ca765072e7538152dbb6498
moondive-cinema <52639320+moondive-cinema@users.noreply.github.com>
2022-09-10 17:47:26 +0900

    edit video and upload page initialize

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,12 +36,37 @@ let videos = [
 
 
 export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser, videos});
+
 export const watch = (req, res) => {
   const {id} = req.params;
   const video = videos[id-1];
   return res.render("watch", {pageTitle: `Watching ${video.title}`, fakeUser, video});
-}
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit", fakeUser});
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+};
+
+export const getEdit = (req, res) => {
+  const {id} = req.params;
+  const video = videos[id-1];
+  return res.render("edit", {pageTitle: `Editing ${video.title}`, fakeUser, video});
+};
+export const postEdit = (req, res) => {
+  const {id} = req.params;
+  const {title} = req.body;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", {pageTitle: "Upload Video", fakeUser});
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+} 
