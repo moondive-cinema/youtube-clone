175ea14e499436431a0236e14d90005edc6faecd
moondive-cinema <moondive79@gmail.com>
2022-09-30 14:14:45 +0900

    duration & current time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,8 +2,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -17,36 +18,54 @@ const handlePlayClick = () => {
     playBtn.innerText = video.paused ? "Play" : "Pause";
   };
 
-  const handleMuteClick = () => {
-    if (video.muted) {
-      video.muted = false;
-    } else {
-      video.muted = true;
-    }
-    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : volumeValue;
-  };
+const handleMuteClick = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
   
-  const handleVolumeChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    if (video.muted) {
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+
+  if (Number(value) === 0) {
+      muteBtn.innerText = "Unmute";
+      video.muted = true;
+      } else {
       video.muted = false;
       muteBtn.innerText = "Mute";
-    }
-    volumeValue = value;
-    video.volume = value;
+      }
+};
 
-    if (Number(value) === 0) {
-        muteBtn.innerText = "Unmute";
-        video.muted = true;
-        } else {
-        video.muted = false;
-        muteBtn.innerText = "Mute";
-        }
+
+const formatTime = (seconds) => {
+  const startIdx = seconds >= 3600 ? 11 : 14;
+  return new Date(seconds * 1000).toISOString().substring(startIdx, 19);
   };
+
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
